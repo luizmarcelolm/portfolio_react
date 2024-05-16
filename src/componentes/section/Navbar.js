d7a2ac9a21123db9c739c36fa8b3146f9e4d9d4d
luiz <luizmarcelolm@hotmail.com>
2024-05-16 17:28:23 -0300

    alt info

--- src/componentes/section/Navbar.js
+++ src/componentes/section/Navbar.js
@@ -13,7 +13,7 @@ function Navbar(){
                 <li className={styles.menu}><Nav.Link className={styles.item} href="#Projects">Projetos</Nav.Link></li>
             </ul>
             <ul> 
-                <ButtonC link="https://drive.google.com/file/d/1_rFpevxiDgadApU4lWSdGIdET9fNMEJu/view?usp=sharing" text='Baixe meu CV'/>
+                <ButtonC link="https://drive.google.com/file/d/1GuvLi8z0iFWrpBuyL0kVkL2-EFRlGR8s/view?usp=sharing" text='Baixe meu CV'/>
                 <li><a href='https://www.linkedin.com/in/luizmarcelomota/' target="_blank"><FaLinkedin size={40}/></a></li>
                 <li><a href='https://github.com/luizmarcelolm' target="_blank"><FaGithubSquare size={40}/></a></li>
             </ul>
